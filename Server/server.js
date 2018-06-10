325f8dc0f620d104aa10bdc15fd176099e1dc60e
Jayden <jaydenphom@gmail.com>
2018-06-10 20:34:15 +1000

    Initial Commits

---
+++ Server/server.js
@@ -0,0 +1,20 @@
+// Calling All Required Plug-ins
+const http = require('http');
+const express = require('express');
+
+const app = express();
+
+const clientPath = `${__dirname}/../client`;
+console.log(`Serving static from ${clientPath}`);
+
+app.use(express.static(clientPath));
+
+const server = http.createServer(app);
+
+server.on('error', (err) => {
+  console.error('Server error: ', err);
+});
+
+server.listen(8080, () => {
+  console.log('Server Started Successfully on 8080!');
+});
