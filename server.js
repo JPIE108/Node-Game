5ac1487e36641e5509963502f8c8f8400fc07d9f
JPIE108 <39828710+JPIE108@users.noreply.github.com>
2018-06-15 08:34:21 +1000

    server.js hotfix

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const socketio = require('socket.io');
 
 const app = express();
 
-const clientPath = `${__dirname}/../client`;
+const clientPath = `${__dirname}`;
 console.log(`Serving static from ${clientPath}`);
 
 app.use(express.static(clientPath));
